bfb82020efcf37a991ea26c6394d01c4d264c944
Mr. Daehn <jdaehn@missouristate.edu>
2018-04-10 12:31:33 -0500

    Initial solution.

--- hw04.cpp
+++ hw04.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <cstdlib>
+#include <cctype>
 #include <iostream>
 #include <string>
 
@@ -16,11 +17,28 @@ std::string convertToMilitaryTime(std::string);
 int main(int argc, char* argv[])
 {
     /* TODO: Prompt user and print converted time. */
+  std::cout << "Enter time: ";
+  std::string standardTime;
+  std::getline(std::cin, standardTime);
+  std::cout << std::endl;
+  std::cout << "Corresponding military time is "
+	    << convertToMilitaryTime(standardTime)
+	    << " hours" << std::endl;
     return EXIT_SUCCESS;
 }
 
 std::string convertToMilitaryTime(std::string st)
 {
-    /* TODO: Implement me correctly */
-    return "";
+  std::size_t colonPosition{st.find(":")};
+  std::string hour{st.substr(0, colonPosition)};
+  std::string min{st.substr(colonPosition + 1, st.find(" ") - colonPosition - 1)};
+  std::string timeOfDay{st.substr(st.length() - 2)};
+  if (tolower(timeOfDay[0]) == 'p') {
+    int iHour{stoi(hour) + 12};
+    hour = std::to_string(iHour);
+  }
+  if (stoi(hour) < 10) {
+    hour = "0" + hour;
+  }
+    return hour + min;
 }
